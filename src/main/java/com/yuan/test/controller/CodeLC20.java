3e186f86eb428b7f9a5320a7f0afb4cdb21807a9
yuan <13038750070@163.com>
2025-02-07 22:48:29 +0800

    LC20.有效的括号补充

--- src/main/java/com/yuan/test/controller/CodeLC20.java
+++ src/main/java/com/yuan/test/controller/CodeLC20.java
@@ -2,18 +2,50 @@ package com.yuan.test.controller;
 
 import java.util.Stack;
 
-public class Code {
-
-
-    public static void main(String[] args) {
-        System.out.println(isValid("()[]{}"));
-    }
+public class CodeLC20 {
 
     /**
-     * LC20. 有效的括号
-     * @param s
-     * @return
+     * LC20.有效的括号
+     *
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     *
+     * 有效字符串需满足：
+     *
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 每个右括号都有一个对应的相同类型的左括号。
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：s = "()"
+     *
+     * 输出：true
+     *
+     * 示例 2：
+     *
+     * 输入：s = "()[]{}"
+     *
+     * 输出：true
+     *
+     * 示例 3：
+     *
+     * 输入：s = "(]"
+     *
+     * 输出：false
+     *
+     * 示例 4：
+     *
+     * 输入：s = "([])"
+     *
+     * 输出：true
      */
+
+    public static void main(String[] args) {
+//        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("([])"));
+    }
+
     public static boolean isValid(String s) {
         //如果字符是奇数则不用判断
         if(s.length()%2 == 1){
