062e716402ad9ccccaf31562f8b61f5aa05f820f
yuan <13038750070@163.com>
2025-02-10 20:18:35 +0800

    LC349. 两个数组的交集

---
+++ src/main/java/com/yuan/suanfa/practice/CodeLC349.java
@@ -0,0 +1,64 @@
+package com.yuan.suanfa.practice;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * LC349. 两个数组的交集
+ *
+ *给定两个数组 nums1 和 nums2 ，返回 它们的
+ * 交集
+ *  。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 示例 2：
+ *
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ * 解释：[4,9] 也是可通过的
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ */
+public class CodeLC349 {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<Integer>();
+        Set<Integer> set2 = new HashSet<Integer>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            set2.add(num);
+        }
+        return intersect(set1, set2);
+    }
+
+    public static int[] intersect(Set<Integer> set1, Set<Integer> set2) {
+        if (set1.size() > set2.size()) {
+            return intersect(set2, set1);
+        }
+        Set<Integer> intersectionSet = new HashSet<Integer>();
+        for (int num : set1) {
+            if (set2.contains(num)) {
+                intersectionSet.add(num);
+            }
+        }
+        int[] intersection = new int[intersectionSet.size()];
+        int index = 0;
+        for (int num : intersectionSet) {
+            intersection[index++] = num;
+        }
+        return intersection;
+    }
+
+}
