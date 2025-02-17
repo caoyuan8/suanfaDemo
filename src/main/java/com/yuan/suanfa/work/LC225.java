2a26d35b231f3ef0cc4dbbb74a0389cc982daf4d
yuan <13038750070@163.com>
2025-02-17 16:45:49 +0800

    LC225.用队列实现栈

---
+++ src/main/java/com/yuan/suanfa/work/LC225.java
@@ -0,0 +1,80 @@
+package com.yuan.suanfa.work;
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * LC225.用队列实现栈
+ *
+ *请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+ *
+ * 实现 MyStack 类：
+ *
+ * void push(int x) 将元素 x 压入栈顶。
+ * int pop() 移除并返回栈顶元素。
+ * int top() 返回栈顶元素。
+ * boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+ *
+ *
+ * 注意：
+ *
+ * 你只能使用队列的标准操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+ * 你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+ *
+ *
+ * 示例：
+ *
+ * 输入：
+ * ["MyStack", "push", "push", "top", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * 输出：
+ * [null, null, null, 2, 2, false]
+ *
+ * 解释：
+ * MyStack myStack = new MyStack();
+ * myStack.push(1);
+ * myStack.push(2);
+ * myStack.top(); // 返回 2
+ * myStack.pop(); // 返回 2
+ * myStack.empty(); // 返回 False
+ *
+ *
+ * 提示：
+ *
+ * 1 <= x <= 9
+ * 最多调用100 次 push、pop、top 和 empty
+ * 每次调用 pop 和 top 都保证栈不为空
+ *
+ */
+public class LC225 {
+    Deque<Integer> q1;
+    Deque<Integer> q2;
+    public LC225() {
+        q1 = new ArrayDeque<>();
+        q2 = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        q2.offer(x);
+        while (!q1.isEmpty()) {
+            int ele = q1.poll();
+            q2.offer(ele);
+        }
+        q1 = q2;
+        q2 = new ArrayDeque<>();
+    }
+
+    public int pop() {
+        return q1.poll();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+
+}
